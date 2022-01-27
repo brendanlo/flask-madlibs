b5e998a6e1806809c4c3328753a1e0bdb8dced57
brendan <lobrendan@gmail.com>
2022-01-26 16:47:55 -0800

    updated questions.html, tried passing silly_story to the template

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+
 @app.get('/')
 def load_silly_story_form():
     """return the madlibs homepage with the story form"""
-
-    return render_template("questions.html")
-
+    breakpoint()
+    prompts = silly_story.prompts
+# define prompts here, pass in render_template
+    return render_template("questions.html", prompts=silly_story.prompts)
