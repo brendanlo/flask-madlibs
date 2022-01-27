4b2bfa294a0f7eea05f585835a02c7b837a8f523
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-01-26 16:04:53 -0800

    adds load_silly_story_form view func

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/')
+def load_silly_story_form():
+    """return the madlibs homepage with the story form"""
+
+    return render_template("questions.html")
+
